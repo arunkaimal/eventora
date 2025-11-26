35d2c2f0d2dd7352dd15b093d1422fd43b071cd5
arunkaimal <arununnikrishnankaimal@gmail.com>
2025-11-26 15:59:10 +0530

    basic things

---
+++ components/Header.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import React from "react";
+import eventora_logo from "@/public/eventora_logo.png";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="fixed top-0 left-0 right-0 z-20 bg-[#191654]/10">
+        <div className="max-w-7xl mx-auto px-6 h-full flex items-center justify-between">
+          {/* Logo */}
+          <div className="relative w-25 h-20">
+            <Link href={"/"} className="flex items-center">
+              <Image
+                src={eventora_logo}
+                fill
+                alt="Eventora Logo"
+                priority 
+              />
+            </Link>
+          </div>
+
+          {/* Search and location - Desktop only */}
+
+          {/* Right side actions */}
+        </div>
+
+        {/* Mobile search and location - Below headers */}
+      </nav>
+
+      {/* Modals */}
+    </>
+  );
+};
+
+export default Header;
